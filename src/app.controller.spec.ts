3e93b310e70c58fa6d4e2b4e6839a8ee36c05598
sadiqasg <sadiqasg@gmail.com>
2025-07-26 22:00:12 +0100

    added unit testing and readme coverage

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello ESG Horizon!"', () => {
+      expect(appController.getHello()).toBe('Hello ESG Horizon!');
     });
   });
 });
