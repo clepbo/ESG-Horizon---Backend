72dfb3cf3855e6b11bf066bf56913c80de07e982
Kelechi Ugwu <kcblack22@gmail.com>
2025-07-30 15:13:17 +0100

    All the OTP services

--- src/otp/otp.module.ts
+++ src/otp/otp.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { OtpService } from './otp.service';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { EmailService } from 'src/email/email.service';
 
 @Module({
-    providers: [OtpService],
+    providers: [OtpService, PrismaService, EmailService],
     exports: [OtpService],
 })
 export class OtpModule {}
