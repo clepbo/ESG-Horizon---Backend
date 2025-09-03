5f2b01008c5896c78896449dda1bf95bcd9ef8ef
Kelechi Ugwu <kcblack22@gmail.com>
2025-09-03 16:53:11 +0100

    super admin dashboard

--- src/user/admin.service.ts
+++ src/user/admin.service.ts
@@ -2,73 +2,12 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 // import { CreateUserDto } from './dto/create-user.dto';
 // import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { UpdateMeDto } from './dto/update-me.dto';
-import { Prisma } from '@prisma/client';
 
 @Injectable()
-export class UserService {
+export class AdminService {
   constructor(private prisma: PrismaService) {}
 
-  findMe(userId: number) {
-    return this.prisma.user.findUnique({
-      where: { id: userId },
-      select: {
-        email: true,
-        first_name: true,
-        last_name: true,
-        phone_number: true,
-        profile_photo_url: true,
-        status: true,
-        last_login: true,
-        created_at: true,
-        updated_at: true,
-        company: {
-          select: { name: true, company_logo_url: true },
-        },
-        department: {
-          select: { name: true },
-        },
-        role: {
-          select: { name: true },
-        },
-      },
-    });
-  }
-
-  async updateMe(userId: number, dto: UpdateMeDto) {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { role, ...rest } = dto;
-
-    const updateData: Prisma.UserUpdateInput = {};
-
-    if (rest.first_name !== undefined) updateData.first_name = rest.first_name;
-    if (rest.last_name !== undefined) updateData.last_name = rest.last_name;
-    if (rest.email !== undefined) updateData.email = rest.email;
-    if (rest.phone_number !== undefined)
-      updateData.phone_number = rest.phone_number;
-
-    return this.prisma.user.update({
-      where: { id: userId },
-      data: updateData,
-    });
-  }
-
-  async getAllPlatformUsers() {
-    return this.prisma.user.findMany({
-      include: {
-        company: true,
-        role: { select: { name: true } },
-        department: { select: { name: true } },
-      },
-      orderBy: { created_at: 'desc' },
-    });
-  }
-
-  async getAllUserRoles() {
-    return this.prisma.role.findMany();
-  }
-
-
+ 
 async getMonthlyRevenue(year: number, month: number) {
   const startOfMonth = new Date(year, month - 1, 1);
   const endOfMonth = new Date(year, month, 0, 23, 59, 59, 999);
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { AdminService } from './admin/admin.service';
+import { AdminServiceTsService } from './admin.service.ts/admin.service.ts.service';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, AdminService, AdminServiceTsService],
 })
 export class UserModule {}
