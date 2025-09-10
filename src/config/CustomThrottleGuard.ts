5cf5ca041e5a5897eac13ebe3f13f3afa5da6cdb
Kelechi Ugwu <kcblack22@gmail.com>
2025-09-10 14:42:55 +0100

    Handled ttl better

--- src/config/CustomThrottleGuard.ts
+++ src/config/CustomThrottleGuard.ts
@@ -7,7 +7,15 @@ export class CustomThrottlerGuard extends ThrottlerGuard {
     context: ExecutionContext,
     throttlerLimitDetail: ThrottlerLimitDetail,
   ): Promise<void> {
-    const timeToWait = Math.ceil(throttlerLimitDetail.timeToExpire / 1000);
-    throw new ThrottlerException(`Too many requests. Please wait ${timeToWait} seconds before retrying.`);
+    // fallback: if timeToExpire is too small, use the full ttl
+    let timeToWait = Math.ceil(throttlerLimitDetail.timeToExpire / 1000);
+
+    if (timeToWait <= 1) {
+      timeToWait = Math.ceil(throttlerLimitDetail.ttl / 1000);
+    }
+
+    throw new ThrottlerException(
+      `Too many requests. Please wait ${timeToWait} seconds before retrying.`,
+    );
   }
 }
