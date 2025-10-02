c21ed6e8c5a1b8fe7948731a26694a238666e68b
sadiqasg <sadiqasg@gmail.com>
2025-10-02 20:00:33 +0100

    revert: add facade to comptn module

--- src/assessment/computation/computation.module.ts
+++ src/assessment/computation/computation.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 
 import { ComputationController } from './computation.controller';
-import { Scope1ComputationService, Scope2Computation, Scope3ComputationService } from './computation.service';
+import {
+  Scope1ComputationService,
+  Scope2Computation,
+  Scope3ComputationService,
+} from './computation.service';
+import { ComputationFacade } from './computation.facade';
 
 @Module({
   controllers: [ComputationController],
-  providers: [Scope1ComputationService,Scope2Computation, Scope3ComputationService],
+  providers: [
+    Scope1ComputationService,
+    Scope2Computation,
+    Scope3ComputationService,
+    ComputationFacade,
+  ],
+  exports: [ComputationFacade],
 })
 export class ComputationModule {}
