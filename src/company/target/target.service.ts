b8cb579f908309ca472dede0ae15f4c2b74ecf99
sadiqasg <sadiqasg@gmail.com>
2025-11-12 08:21:34 +0100

    cleanup

--- src/company/target/target.service.ts
+++ src/company/target/target.service.ts
@@ -65,9 +65,9 @@ export class TargetService {
         generalTarget: {
           create: {
             reductionPercentage: data.reductionPercentage,
-            baselineYearEmission: data.baselineYearEmission,
-            targetEmission: data.targetEmission,
-            currentEmission: data.currentEmission
+            // baselineYearEmission: data.baselineYearEmission,
+            // targetEmission: data.targetEmission,
+            // currentEmission: data.currentEmission
           },
         },
       },
