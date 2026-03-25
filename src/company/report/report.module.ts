32a86f489989faea70c3bbd063e6f3eec41d2197
sadiqasg <sadiqasg@gmail.com>
2026-03-25 09:26:51 +0100

    feat: import ScoringModule into ReportModule

--- src/company/report/report.module.ts
+++ src/company/report/report.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ReportService } from './report.service';
 import { ReportController } from './report.controller';
+import { ScoringModule } from '../../assessment/scoring/scoring.module';
 
 @Module({
+  imports: [ScoringModule],
   controllers: [ReportController],
   providers: [ReportService],
   exports: [ReportService],
