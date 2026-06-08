c4cfaa4208b4e709339a7be069e57489c3ae92ca
Sadiq A Sambo <sadiqasg@gmail.com>
2026-06-08 12:09:36 +0100

    Update app.service.ts
    Added health endpoint

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly prisma: PrismaService) {}
+
   getHello(): string {
     return 'Hello ESG Horizon!';
+  }
+
+  async checkDatabaseHealth() {
+    // This fires an ultra-light raw query directly to Postgres.
+    // It forces an active database lookup without reading/writing table rows.
+    await this.prisma.$queryRaw`SELECT 1`;
+    return {
+      status: 'UP',
+      timestamp: new Date().toISOString(),
+    };
   }
 }
